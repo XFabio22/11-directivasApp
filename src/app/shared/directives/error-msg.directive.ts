fa736126ac764750e695485c28375cf2260fe45f
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2023-02-25 15:37:24 +0100

    directive cambiar color de texto

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,18 +1,25 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrorMsgDirective implements OnInit{
 
-  constructor() { 
+  htmlElement: ElementRef<HTMLElement>;
+  @Input() color:string ='red';
+  constructor( private el:ElementRef<HTMLElement>) { 
     console.log('constructor directive');
-    
+    console.log(el);
+    this.htmlElement =el
   }
 
   ngOnInit(): void {
     console.log('NgOnInit directive');
+    this.setColor();
     
+  }
+  setColor():void{
+    this.htmlElement.nativeElement.style.color= this.color
   }
 
 }
