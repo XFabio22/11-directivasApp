5cbb6e9c5811de0626b89eb193fbe394ef1c5171
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2023-02-26 21:53:14 +0100

    Cambiar el mensaje

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -7,6 +7,7 @@ export class ErrorMsgDirective implements OnInit{
 
   htmlElement: ElementRef<HTMLElement>;
   @Input() color:string ='red';
+  @Input() mensaje:string = ''
   constructor( private el:ElementRef<HTMLElement>) { 
     console.log('constructor directive');
     console.log(el);
@@ -16,10 +17,17 @@ export class ErrorMsgDirective implements OnInit{
   ngOnInit(): void {
     console.log('NgOnInit directive');
     this.setColor();
+    this.setMensaje();
     
   }
   setColor():void{
-    this.htmlElement.nativeElement.style.color= this.color
+    this.htmlElement.nativeElement.style.color= this.color;
+    this.htmlElement.nativeElement.classList.add('form-text')
+
+  }
+
+  setMensaje():void{
+    this.htmlElement.nativeElement.innerText = this.mensaje;
   }
 
 }
